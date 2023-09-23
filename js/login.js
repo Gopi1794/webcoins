6a7f23c11fab47dd2d12f21f6a8fa6d6db2d1e67
Gabriel Gomez <Gopi1794@users.noreply.github.com>
2023-09-23 03:25:07 -0300

    prueba de commir con vs

--- js/login.js
+++ js/login.js
@@ -9,17 +9,37 @@ const caja_trasera_login = document.querySelector('.box_back_login');
 const caja_trasera_register = document.querySelector('.box_back_Registrer');
 
 function register() {
-    formulario_register.style.display = "block";
+    if (window.innerWidth > 850){
+        formulario_register.style.display = "none";
+        contenedor_register.style.left = "410px";
+        formulario_login.style.display = "none";
+        caja_trasera_register.style.opacity = "0";
+        caja_trasera_login.style.opacity = "1";
+    }
+    else{
+        formulario_register.style.display = "none";
     contenedor_register.style.left = "410px";
-    formulario_login.style.display = "none";
-    caja_trasera_register.style.opacity = "0";
-    caja_trasera_login.style.opacity = "1";
+    formulario_login.style.display = "block";
+    caja_trasera_register.style.opacity = "block";
+    caja_trasera_login.style. = "1";
+    }
+    
 }
 function iniciarSesion() {
-    formulario_register.style.display = "none";
-    contenedor_register.style.left = "10px";
-    formulario_login.style.display = "block";
-    caja_trasera_register.style.opacity = "1";
-    caja_trasera_login.style.opacity = "0";
+    if (window.innerWidth > 850){
+        formulario_register.style.display = "none";
+        contenedor_register.style.left = "10px";
+        formulario_login.style.display = "block";
+        caja_trasera_register.style.opacity = "1";
+        caja_trasera_login.style.opacity = "0";
+    }
+    else{
+        formulario_register.style.display = "none";
+        contenedor_register.style.left = "10px";
+        formulario_login.style.display = "block";
+        caja_trasera_register.style.opacity = "block";
+        caja_trasera_login.style.opacity = "0";
+    }
+    
 }
 
