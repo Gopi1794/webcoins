0eeb69393bae86539cabbf868793a95e59d6b9ff
Gabriel Gomez <Gopi1794@users.noreply.github.com>
2023-10-06 19:39:47 -0300

    nuevo

--- php/registro_usuarios_be.php
+++ php/registro_usuarios_be.php
@@ -10,19 +10,55 @@ $contrasena = $_POST['contrasena'];
 $query = "INSERT INTO user (nombre_completo, email, user, contrasena)
         VALUES ('$nombre_completo' , '$email' , '$user' , '$contrasena')";
 
-$ejecutar = mysqli_query($conexion , $query);
+//verificar que el correo no se repita en la base de datos
 
-if ($ejecutar){
-    echo'
+$verificar_correo = mysqli_query ($conexion , "SELECT * FROM user WHERE email = '$email'");
+
+if (mysqli_num_rows($verificar_correo) > 0) {
+    echo '
     <script>
-    alert("Registro exitoso");
-    window.location = "../login.php";
+    alert("El correo electrónico ya está registrado. Por favor, utiliza otro.");
+    window.location = "../login.php"; // Redirige a la página de registro nuevamente
     </script>';
-}else{
-    echo'
+    exit();
+}
+
+$verificar_user = mysqli_query ($conexion , "SELECT * FROM user WHERE email = '$user'");
+
+if (mysqli_num_rows($verificar_user) > 0) {
+    echo '
     <script>
-    alert("Intentalo nuevamente , usuario no registro exitoso");
-    window.location = "../login.php";
+    alert("El nombre de usuario no disponible. Por favor, utiliza otro.");
+    window.location = "../login.php"; // Redirige a la página de registro nuevamente
+    </script>';
+    exit();
+}
+
+ // Utiliza una consulta preparada para evitar la inyección SQL
+if ($stmt = mysqli_prepare($conexion, $query)) {
+    mysqli_stmt_bind_param($stmt, "ssss", $nombre_completo, $email, $user, $contrasena);
+    $ejecutar = mysqli_stmt_execute($stmt);
+
+    if ($ejecutar) {
+        echo '
+        <script>
+        alert("Registro exitoso");
+        window.location = "../login.php";
+        </script>';
+    } else {
+        echo '
+        <script>
+        alert("Inténtalo nuevamente, registro no exitoso");
+        window.location = "../login.php"; // Redirige a la página de registro nuevamente
+        </script>';
+    }
+
+    mysqli_stmt_close($stmt);
+} else {
+    echo '
+    <script>
+    alert("Error en la consulta preparada");
+    window.location = "../login.php"; // Redirige a la página de registro nuevamente
     </script>';
 }
 
